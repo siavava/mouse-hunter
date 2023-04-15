5ad5f8b0295b8294652fc62a2464fdf62102f862
siavava <amittaijoel@outlook.com>
2023-04-15 01:09:33 -0400

    change blob animation, add comments, clock, etc.

--- src/index.js
+++ src/index.js
@@ -7,33 +7,105 @@
 import $ from 'jquery';
 import './style.scss';
 
+const circle = document.getElementById('ring');
+const clock = document.getElementById('main');
+// const progressDot = document.getElementById('progress-dot');
+function Counter() {
+  this.count = 0;
+
+  this.minutes = 0;
+  this.seconds = 0;
+
+  this.tick = () => {
+    // this.count += 1;
+    this.seconds += 1;
+    if (this.seconds === 60) {
+      this.minutes += 1;
+      this.seconds = 0;
+    }
+    this.renderCount();
+  };
+
+  this.renderCount = () => {
+    $('#minutes').html(this.minutes > 9 ? `${this.minutes}` : `0${this.minutes}`);
+    $('#seconds').html(this.seconds > 9 ? `${this.seconds}` : `0${this.seconds}`);
+    // move the progress along the circle
+    // It should start at 0 when this.tick is 0 and end at 360 when this.tick is 60
+
+    const progress = this.minutes * 360 + this.seconds * 6;
+    circle.animate({
+      transform: `rotate(${progress}deg)`,
+    }, {
+      duration: 1000,
+      fill: 'forwards',
+    }, 'ease-in-out');
+
+    clock.animate({
+      transform: `rotate(${-progress}deg)`,
+    }, {
+      duration: 1000,
+      fill: 'forwards',
+    }, 'ease-in-out');
+  };
+
+  this.start = () => {
+    setInterval(this.tick, 1000);
+  };
+}
+
 console.log('Starting up...');
-$('#main').html('Here we go!');
 
-let count = 0;
-const renderCount = () => {
-  $('#main').html(`You have been on this page for ${count} seconds.`);
-};
+new Counter().start();
 
-const tick = () => {
-  count += 1;
-  renderCount();
-};
-
-setInterval(tick, 1000);
-
-const body = $('#body');
-// const tracker = $('#tracker');
-const tracker = document.getElementById('tracker');
+const body = document.getElementById('body');
 
 // follow mouse pointer with animation
-body.on('pointermove', (event) => {
+body.onpointermove = (event) => {
+  const blob = document.getElementById('tracker');
+  const cursor = document.getElementById('cursor');
   const { pageX, pageY } = event;
-  tracker.animate({
-    top: `${pageY - 200}px`,
-    left: `${pageX - 200}px`,
+  cursor.animate({
+    top: `${pageY}px`,
+    left: `${pageX}px`,
+  }, {
+    duration: 100,
+    fill: 'forwards',
+  }, 'ease-in-out');
+
+  const elemWidth = blob.offsetWidth;
+  const elemHeight = blob.offsetHeight;
+  const elemX = pageX - elemWidth / 2;
+  const elemY = pageY - elemHeight / 2;
+
+  blob.animate({
+    top: `${elemY}px`,
+    left: `${elemX}px`,
   }, {
     duration: 3000,
     fill: 'forwards',
   }, 'ease-in-out');
+};
+
+// load data/quotes.json
+$.getJSON('data/quotes.json', (data) => {
+  const quote = document.getElementById('quote');
+  const author = document.getElementById('author');
+
+  const cycle = () => {
+    const currentIndex = Math.floor(Math.random() * data.length);
+    const currentQuote = data[currentIndex];
+
+    quote.classList.add('pre-animation');
+    setTimeout(() => {
+      quote.innerHTML = currentQuote.quote;
+      author.innerHTML = currentQuote.person;
+    }, 1000);
+
+    setTimeout(() => {
+      quote.classList.remove('pre-animation');
+    }, 1000);
+  };
+
+  cycle();
+  setInterval(cycle, 10000);
 });
