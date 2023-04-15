f22bd3ff8b6f40fdd63aa1966534a42763c5b84b
siavava <amittaijoel@outlook.com>
2023-04-15 01:34:46 -0400

    ...

--- src/index.js
+++ src/index.js
@@ -87,7 +87,7 @@ body.onpointermove = (event) => {
 };
 
 // load data/quotes.json
-$.getJSON('/data/quotes.json', (data) => {
+$.getJSON('data/quotes.json', (data) => {
   const quote = document.getElementById('quote');
   const author = document.getElementById('author');
 
